7d1d74a61ff87b70595cf1236a7ca72f28c4e81e
Jen Chartier <54649817+JChartier001@users.noreply.github.com>
2020-01-24 01:09:11 -0600

    Styling and split up less files

--- src/components/Patient/PatientRegister.js
+++ src/components/Patient/PatientRegister.js
@@ -63,20 +63,19 @@ const PatientRegister = (props) => {
     console.log("patient", patient)
 
     return(
-        <div className="patientRegister">
-            <Form onSubmit={handleSubmit}>
+        <div className="patientRegister">            
                 <Row >                    
-                    <Col xs={6} md={4}>
+                    <Col xs={6} >
                         <Form.Label>City</Form.Label>
                         <Form.Control className="input" type="text" name="city" placeholder="City" onChange={handleInput}/>
                         </Col>  
-                        <Col xs={6} md={4}>
+                        <Col xs={6}>
                         <Form.Label>State</Form.Label>
                         <Form.Control className="input" type="text" name="state" placeholder="State" onChange={handleInput}/>
                     </Col>                    
                 </Row>
                 <Row>
-                    <Col xs={6} >
+                    {/* <Col xs={4} > */}
                 <Form.Label >Insurance Type</Form.Label>
                 <Form.Control as="select" name='insurance' onChange={handleInput} defaultValue=''>                
                     <option value='' disabled hidden>Please Select</option>
@@ -84,17 +83,17 @@ const PatientRegister = (props) => {
                     <option value="Private Insurance">Private Insurance</option>
                     <option value="Other">Other</option>     
                 </Form.Control> 
-                </Col>
-                <Col xs={6}>
+                {/* </Col>
+                <Col xs={8}> */}
                 <Form.Label >In need of a Case Manager?</Form.Label>
                 <Form.Control as="select"  name='case_manager' onChange={handleInput} defaultValue=''>                
                     <option value='' disabled hidden>Please Select</option>
                     <option value={true}>Yes</option>
                     <option value={false}>No</option>    
                 </Form.Control> 
-                </Col>
+                {/* </Col> */}
                 </Row>
-                
+                <div className="check">
                     <Form.Label>Please check the needs of your child/self:</Form.Label>
                     <Form.Check type="checkbox" label="Trach" name='trach' value="true" onChange={handleInput}/>
                     <Form.Check type="checkbox" label="Vent" name='vent' value="true" onChange={handleInput}/>
@@ -103,18 +102,23 @@ const PatientRegister = (props) => {
                     <Form.Check type="checkbox" label="Verbal" name='verbal' value="true" onChange={handleInput}/>
                     <Form.Check type="checkbox" label="Non-verbal" name='verbal' value="false" onChange={handleInput}/>
                     <Form.Check type="checkbox" label="Attends School" name='school' value="true" onChange={handleInput}/>
-                    <Row>
+                    </div >
+                    {/* <Row> */}
                     <Form.Label >If yes, what times does your child attend school?
                         <Form.Control className="input" type="text" name="school_time" onChange={handleInput}/>
                         </Form.Label>
-                    </Row>
+                    {/* </Row> */}
                     <Form.Label >What are your child/self's mobility needs
                     <Form.Control className="input" type="text" name="mobility" onInput={handleInput}/>
                 </Form.Label>
+                <div className="check">
                 <Form.Check type="checkbox" label="Nurse to attend appointments?" name='appt' value="true" onChange={handleInput}/>
                 <Form.Check type="checkbox" label="If yes, will nurse need to use own car?" name='use_car' value="true" onChange={handleInput}/>
+                </div>
+                <div className="check animal">
+                <Form.Check type="checkbox" label="Animals in the home?" name='animals' value="true" onChange={handleInput}/>
                 
-                <Form.Label >Who does your child/self life with? </Form.Label>
+                <Form.Label >Who does your child/self live with? </Form.Label>
                 <Row>
                     <Col xs={6} md={4}>
                         <Form.Check type="checkbox" label="Self" name='live_with' value="self" onChange={handleInput}/>
@@ -122,10 +126,9 @@ const PatientRegister = (props) => {
                         <Form.Check type="checkbox" label="Others"name='live_with' value="other" onChange={handleInput}/>
                     </Col>
                 </Row>
-               <Form.Check type="checkbox" label="Animals in the home?" name='animals' value="true" onChange={handleInput}/>
-                
+                </div>
+               
                 <Button type="submit" variant="outline-info" onClick={handleSubmit}>Submit</Button>
-                </Form>
         </div>
     )
 }
